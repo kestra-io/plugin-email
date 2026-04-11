b84c6df429fcdfe55aead3f09ae147cdee89b6ef
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 02:51:36 +0530

    refactor: replace io.kestra.core.tasks with io.kestra.plugin.core

--- src/main/java/io/kestra/plugin/email/MailReceivedTrigger.java
+++ src/main/java/io/kestra/plugin/email/MailReceivedTrigger.java
@@ -43,7 +43,7 @@ import lombok.experimental.SuperBuilder;
 
                 tasks:
                   - id: process_email
-                    type: io.kestra.core.tasks.log.Log
+                    type: io.kestra.plugin.core.log.Log
                     message: |
                       New email received:
                       Subject: {{ trigger.latestEmail.subject }}
